51086643a2339ae823af363c3e8340264b24edb0
Kaique Oliveira <32941480+kaique-oli@users.noreply.github.com>
2023-02-09 12:23:27 -0300

    primeiro commit

---
+++ FilmesApi/Controllers/FilmeController.cs
@@ -0,0 +1,40 @@
+﻿using FilmesApi.Data;
+using FilmesApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class FilmeController : ControllerBase
+{
+    private Context _context;
+
+    public FilmeController(Context context)
+    {
+        _context = context;
+    }
+
+    [HttpPost]
+    public IActionResult AdicionaFilme([FromBody] Filme filme)
+    {
+       _context.Filmes.Add(filme);
+        _context.SaveChanges();
+       return CreatedAtAction(nameof(RecuperaFilmesProId), new { id = filme.Id}, filme);
+    }
+    [HttpGet]
+    public IActionResult RecuperaFilmes([FromQuery] int skip, [FromQuery] int take)
+    {
+        return Ok(_context.Filmes.Skip(skip).Take(take));  
+    }
+    [HttpGet("{id}")]
+    public IActionResult RecuperaFilmesProId(int id)
+    {
+        var result = _context.Filmes.FirstOrDefault(filme => filme.Id == id);
+        if (result == null) return NotFound();  
+        return Ok(result);  
+
+    }
+
+}
+        
---
+++ FilmesApi/Data/Context.cs
@@ -0,0 +1,11 @@
+﻿using FilmesApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FilmesApi.Data;
+
+public class Context : DbContext
+{
+    public Context(DbContextOptions<Context> options) : base (options) 
+    { }  
+    public DbSet<Filme>? Filmes { get; set; }
+}
